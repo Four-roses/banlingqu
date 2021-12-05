998d21ab8ebcf1b9e56aa7adaef33a2b60765bb1
Four-roses <921019370@qq.com>
2021-12-05 11:47:06 +0800

    homework1

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+package niu;
+
+public class helloworld { 
+    public static void main(String[] args)
+    {
+          System.out.println("Hello World");
+                              
+    }
+
+
+}
